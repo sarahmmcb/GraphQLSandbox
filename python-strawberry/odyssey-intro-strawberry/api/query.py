04a4ccadba83a1f5649b943627c3d7d11e8520e2
Your Name <you@example.com>
2026-09-17 14:33:10 -0400

    fix nested folder

---
+++ python-strawberry/odyssey-intro-strawberry/api/query.py
@@ -0,0 +1,16 @@
+import strawberry
+from .types.playlist import Playlist
+
+
+@strawberry.type
+class Query:
+
+    @strawberry.field(
+        description="Playlists hand-picked to be featured to all users."
+    )
+    def featured_playlists(self) -> list[Playlist]:
+        return [
+            Playlist(id="1", name="GraphQL Groovin'", description=None),
+            Playlist(id="2", name="Graph Explorer Jams", description=None),
+            Playlist(id="3", name="Interpretive GraphQL Dance", description=None),
+        ]
